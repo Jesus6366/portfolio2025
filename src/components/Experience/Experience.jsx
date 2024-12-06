42473d9ae71ef258d51ebd12bbbfd248dda3786e
unknown <polibos6366@gmail.com>
2024-12-05 21:05:31 -0700

    skills component done

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,16 +1,17 @@
 import skills from "../../data/skills.json";
 import { getImageUrl } from "../../utils";
+import styles from "./Experience.module.css";
 
 const Experience = () => {
   return (
-    <section id="experience">
-      <h2>Skills / Tools</h2>
-      <div>
-        <div>
+    <section id="experience" className={styles.container}>
+      <h2 className={styles.title}>Skills / Tools</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
           {skills.map((skill, id) => {
             return (
-              <div key={id}>
-                <div>
+              <div key={id} className={styles.sill}>
+                <div className={styles.skillImageContainer}>
                   <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
                 </div>
                 <p>{skill.title}</p>
