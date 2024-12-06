fa368b2832f2e50fafc1e738902bb84ba967f58a
unknown <polibos6366@gmail.com>
2024-12-05 17:07:03 -0700

    navbar styles completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import styles from "./App.module.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  return <div className={styles.App}>hello world</div>;
+  return (
+    <div className={styles.App}>
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
